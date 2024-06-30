a1816525776a9faa26be63ecd0d9461a0c9d96c2
Aneesh Paul <aneesh16117@gmail.com>
2024-06-30 17:44:42 +0530

    Middleware added

---
+++ backend/middleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+
+const { JWT_SECRET } = process.env;
+
+const authMiddleware = (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(403).json({ message: "Authentication issue" });
+  }
+
+  const token = authHeader.split(" ")[1]; // extracting out the token from auth-header
+
+  // verifying the user and setting the user in request after verification
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    req.userId = decoded.userId;
+
+    next();
+  } catch (err) {
+    return res.status(403).json({ error: err.message });
+  }
+};
