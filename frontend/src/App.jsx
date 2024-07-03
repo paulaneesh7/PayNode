bdbed6f2031b9f6d88d12d1f086bd70e9c303646
Aneesh Paul <aneesh16117@gmail.com>
2024-07-03 21:44:46 +0530

    Routes and Pages Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,16 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
+
+const router = createBrowserRouter([
+  { path: "/signup", element: <Signup /> },
+  { path: "/signin", element: <Signin /> },
+  { path: "/dashboard", element: <Dashboard /> },
+  { path: "/send", element: <SendMoney /> },
+]);
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 }
