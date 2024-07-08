7ec82b44629a34ee47a031c2ea3ae05c77b1cf82
Aneesh Paul <aneesh16117@gmail.com>
2024-07-08 17:52:05 +0530

    minor change for deployment

--- client/src/config/URL.js
+++ client/src/config/URL.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:8080/api/v1";
-// export const URL = "https://paynode-xysx.onrender.com/api/v1";
+// export const URL = "http://localhost:8080/api/v1";
+export const URL = "https://paynode-xysx.onrender.com/api/v1";
